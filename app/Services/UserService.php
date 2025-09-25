913c94c64e45cabba72fe84fe2b8017f43822a92
KhaingZinZinZaw1 <khaingzinzinzaw@cgm-myanmar.com>
2025-09-25 15:19:05 +0630

    My changes

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -73,7 +73,7 @@ class UserService implements UserServiceInterface
     public function storeUser(array $data)
     {
         // Add currently logged-in user ID to created_by
-        $data['created_by'] = Auth::id();
+        $data['created_by'] = Auth::id() ?? 1;
 
         // Hash password
         $data['password'] = bcrypt($data['password']);
